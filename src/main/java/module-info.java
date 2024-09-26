7ed0f01ab0a9fa279d758824d1d6590da5e4fc53
TranTuanAnh <vt34023@gmail.com>
2024-09-26 20:41:50 +0700

    first commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module com.jmc.libsystem {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires javafx.graphics;
 
     opens com.jmc.libsystem to javafx.fxml;
     exports com.jmc.libsystem;

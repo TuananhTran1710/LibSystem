0f546ddb750f1529b7ad500f67dfc95165adf6fd
TranTuanAnh <vt34023@gmail.com>
2024-10-21 15:17:52 +0700

    21/10

--- src/main/java/com/jmc/libsystem/QueryDatabase/QueryBorrowHistory.java
+++ src/main/java/com/jmc/libsystem/QueryDatabase/QueryBorrowHistory.java
@@ -1,7 +1,7 @@
 package com.jmc.libsystem.QueryDatabase;
 
 import com.jmc.libsystem.Models.DatabaseDriver;
-
+import com.jmc.libsystem.Views.StateAccount;
 import javafx.scene.control.Alert;
 
 import java.sql.PreparedStatement;
@@ -9,61 +9,29 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class QueryBorrowHistory {
-    public static ResultSet Borrow (String userId) {
-        ResultSet resultSet = null;
-        String query = "SELECT COUNT(*) AS total_borrows " +
-                "FROM borrow_history " +
-                "WHERE user_id = ?";
-        try {
-            PreparedStatement preparedStatement = DatabaseDriver.getConn().prepareStatement(query);
-            preparedStatement.setString(1, userId);
-            resultSet = preparedStatement.executeQuery();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return resultSet;
-    }
-
-    public static ResultSet Return (String userId) {
-        ResultSet resultSet = null;
-        String query = "SELECT COUNT(CASE WHEN return_date IS NOT NULL THEN 1 END) AS total_returns " +
-                "FROM borrow_history " +
-                "WHERE user_id = ?";
-        try {
-            PreparedStatement preparedStatement = DatabaseDriver.getConn().prepareStatement(query);
-            preparedStatement.setString(1, userId);
-            resultSet = preparedStatement.executeQuery();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return resultSet;
-    }
-
-    public static ResultSet getListBorrow (String userId) {
-        ResultSet resultSet = null;
-        String query = "SELECT title, authors, thumbnail_url " +
-                "FROM borrow_history " +
-                "WHERE user_id = ?";
-        try {
-            PreparedStatement preparedStatement = DatabaseDriver.getConn().prepareStatement(query);
-            preparedStatement.setString(1, userId);
-            resultSet = preparedStatement.executeQuery();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return resultSet;
-    }
-  
     public static boolean isBanned(String user_id) {
         String query = "SELECT * FROM borrowhistory WHERE user_id = ? AND DATEDIFF(CURDATE(), borrow_date) > 120";
         try {
-
             PreparedStatement preparedStatement = DatabaseDriver.getConn().prepareStatement(query);
             preparedStatement.setString(1, user_id);
             ResultSet resultSet = preparedStatement.executeQuery();
 
             // Nếu có bản ghi trả về, có nghĩa là có lần mượn quá hạn
             return resultSet.next();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void updateUserBanned() {
+        String query = "SELECT user_id FROM borrowhistory WHERE DATEDIFF(CURDATE(), borrow_date) > 120";
+        try {
+            PreparedStatement preparedStatement = DatabaseDriver.getConn().prepareStatement(query);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                QueryAccountData.updateState(StateAccount.BANNED.toString(), resultSet.getString(1));
+            }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -89,5 +57,6 @@ public class QueryBorrowHistory {
             throw new RuntimeException(e);
         }
     }
-  
+
+
 }

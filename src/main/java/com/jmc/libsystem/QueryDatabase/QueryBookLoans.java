c67b22feec533126576fc25802f0262cb102743b
huyen2k <huyennguyen08032005@gmail.com>
2024-11-01 22:58:07 +0700

    upd

--- src/main/java/com/jmc/libsystem/QueryDatabase/QueryBookLoans.java
+++ src/main/java/com/jmc/libsystem/QueryDatabase/QueryBookLoans.java
@@ -65,7 +65,7 @@ public class QueryBookLoans {
 
     public static ResultSet getTotalLoaned(String userId) {
         ResultSet resultSet = null;
-        String query = "SELECT COUNT(*) AS total_borrows " +
+        String query = "SELECT COUNT(DISTINCT google_book_id) AS total_borrows " +
                 "FROM bookloans " +
                 "WHERE user_id = ?";
         try {
@@ -80,7 +80,9 @@ public class QueryBookLoans {
 
     public static ResultSet getTotalReturned(String userId) {
         ResultSet resultSet = null;
-        String query = "SELECT SUM(return_date IS NOT NULL) AS total_returns FROM bookloans WHERE user_id = ?";
+        String query = "SELECT COUNT(DISTINCT google_book_id) AS total_returns " +
+                "FROM bookloans " +
+                "WHERE user_id = ? AND return_date IS NOT NULL";
         try {
             PreparedStatement preparedStatement = DatabaseDriver.getConn().prepareStatement(query);
             preparedStatement.setString(1, userId);

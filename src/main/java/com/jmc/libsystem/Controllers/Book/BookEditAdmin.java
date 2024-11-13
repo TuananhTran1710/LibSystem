994f8ad2751b5febb8bd2866371d560d16a64b2f
TranTuanAnh <vt34023@gmail.com>
2024-11-13 15:59:24 +0700

    t

---
+++ src/main/java/com/jmc/libsystem/Controllers/Book/BookEditAdmin.java
@@ -0,0 +1,13 @@
+package com.jmc.libsystem.Controllers.Book;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class BookEditAdmin implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
